f5cc15ec0ed70e93e866b22f795ceb4fd9621933
r3versi <fluo392@gmail.com>
2020-07-03 13:11:31 +0200

    Planimetry & room/camera models

--- art_beat/dashboard/models.py
+++ art_beat/dashboard/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    description = models.TextField()
+
+
+class Paintings(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class Camera(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    x = models.FloatField()
+    y = models.FloatField()
+
+
+class CameraVisitors(models.Model):
+    camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
+    visitors = models.IntegerField()
+    datetime = models.DateTimeField(auto_now_add=True)
+
+
+class RoomVisitors(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    visitors = models.IntegerField()
+    datetime = models.DateTimeField(auto_now_add=True)
